086375b1eafaf588160a047ceba24971f340b725
BrevanBender <you@ex.com>
2022-03-29 13:02:28 -0700

    heroku commit

--- app.js
+++ app.js
@@ -43,5 +43,5 @@ app.use("/", postController);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("App Running: " + port);
+  console.log("app running: " + port);
 });
